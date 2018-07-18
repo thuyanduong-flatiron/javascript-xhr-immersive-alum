d707ced1fd504c060d86b077803cf89c863d3aef
thuyanduong-flatiron <thuyan.duong@flatironschool.com>
2018-07-17 22:37:43 -0400

    added fetch solution

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 function getRepositories(){
-  const req = new XMLHttpRequest();
-  req.addEventListener("load", showRepositories);
-  req.open("GET", 'https://api.github.com/users/octocat/repos');
-  req.send();
+  fetch('https://api.github.com/users/octocat/repos')
+  .then(response => response.json())
+  .then(jsonData => {
+    showRepositories(jsonData)
+  })
 }
 
-function showRepositories(event, data) {
-  var repos = JSON.parse(this.responseText)
-  console.log(repos)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+function showRepositories(jsonData) {
+  const repoList = `<ul>${jsonData.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
 
 function getCommits(el) {
   const name = el.dataset.repo
-  const req = new XMLHttpRequest()
-  req.addEventListener("load", showCommits)
-  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
-  req.send()
+  fetch('https://api.github.com/repos/octocat/' + name + '/commits')
+  .then(response => response.json())
+  .then(jsonData => {
+    showCommits(jsonData)
+  })
 }
 
-function showCommits() {
-  const commits = JSON.parse(this.responseText)
-  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+function showCommits(jsonData) {
+  const commitsList = `<ul>${jsonData.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("commits").innerHTML = commitsList
 }
